d15debfa9e78e57a32ff515446f201bb8b0462f7
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2017-11-22 17:10:10 +0000

    update á klasa

--- lok.py
+++ lok.py
@@ -4,30 +4,6 @@
 
 #Pöntun á þjónustu
 
-print("Valmöguleikar:"
-      "\nMálari"
-      "\nPípari"
-      "\nRafvirki"
-      "\nSmiður")
-val = input("Veldu þjónustu: ")
-
-
-
-a = 0
-try:
-    if val == "malari" or val == "Malari" or val == "Málari" or val == "málari":
-        a = 0
-    elif val == "pipari" or val == "Pipari" or val == "Pípari" or val == "pípari":
-        a = 2
-    elif val == "rafvirki" or val == "Rafvirki":
-        a = 3
-    elif val == "smiður" or val == "Smiður":
-        a = 4
-    else:
-        print("Rangur innsláttur")
-except:
-    print("Óvænt villa")
-
 
 class Thjonusta:
     def __init__(self, þjonusta):
@@ -39,10 +15,43 @@ class Thjonusta:
     def pipari(self, lagnir):
         asd = "asd"
 
-    def rafvirki(self, asd):
+    def rafvirki(self, klst):
         asd = "asd"
 
-k1 = Thjonusta(a)
+print("Valmöguleikar:"
+      "\nMálari"
+      "\nPípari"
+      "\nRafvirki"
+      "\nSmiður")
+
+þjon = 0
+val = input("Veldu þjónustu: ")
+k1 = Thjonusta
+if val == "malari" or val == "Malari" or val == "Málari" or val == "málari":
+    þjon = 0
+elif val == "pipari" or val == "Pipari" or val == "Pípari" or val == "pípari":
+    þjon = 2
+    print("\nValmöguleikar:"
+          "\n1. Vaskur"
+          "\n2. Klósett"
+          "\n3. Stórar lagnir (nýjar lagnir fyrir t.d. baðherbergi)"
+          "\n4. Ofn"
+          "\n5. Sturta/Bað")
+    try:
+        verk = int(input("Veldu verk (1-5): "))
+        if verk == 1:
+            k1(þjon).malari(1)
+    except ValueError:
+        print("Rangt gagnatak")
+
+
+elif val == "rafvirki" or val == "Rafvirki":
+    þjon = 3
+elif val == "smiður" or val == "Smiður":
+    þjon = 4
+else:
+    print("Rangur innsláttur")
+
 
 
 
