b70cda961591a08b69ce5eb37b3c0ee4e730e036
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2017-12-01 09:21:51 +0000

    kul

--- lok.py
+++ lok.py
@@ -121,7 +121,89 @@ while asd:
         elif val == "rafvirki" or eval(val) == 3:
             þjon = 3
         elif val == "smiður" or val == "smidur" or eval(val) == 4:
+            val == "smidur" or val == "Smidur":
             þjon = 4
+            print("\nValmöguleikar:"
+                  "\n1. Utandyra"
+                  "\n2. Innandyra")
+            try:
+                utin = int(input("Veldu annað hvort: "))
+
+                if utin == 1:
+                    print("\nValmöguleikar:"
+                          "\n1. Þak"
+                          "\n2. Gluggar"
+                          "\n3. Pallur")
+                    utinn = int(input("Veldu verk (1-3): "))
+                    if utinn == 1:
+                        st = int(input("Hver er stærð þaksins í fermetrum? "))
+                        fm = 1200 + 2000
+                        skurt = fm * st
+                        print(skurt)
+                    elif utinn == 2:
+                        fj = int(input("Hversu margir eru gluggarnir? "))
+                        teljari = 0
+                        jolasveinn = []
+                        for x in range(fj):
+                            teljari += 1
+                            print("--- Gluggi", teljari, "---")
+                            ster1 = int(input("Sláðu inn hæð glugga í cm "))
+                            ster2 = int(input("Sláðu inn breidd glugga í cm "))
+                            ster3 = ster1 * ster2
+                            ster3 = ster3 * 1000
+                            jolasveinn.append(ster3)
+                        jolatre = 3000
+                        for x in jolasveinn:
+                            jolatre = x + jolatre
+                        print(jolatre)
+
+                    elif utinn == 3:
+                        pall = int(input("Sláðu inn stærð palls í fermetrum "))
+                        vegg = input("Viltu vegg? J/N").upper()
+                        if vegg == "J":
+                            ha = int(input("Hversu hár á hann að vera? "))
+                    else:
+                        print("Rangur innsláttur")
+
+                elif utin == 2:
+                    print("\nValmöguleikar:"
+                          "\n1. Veggur"
+                          "\n2. Innrétting"
+                          "\n3. Gólf")
+                    utinn = int(input("Veldu verk (1-3): "))
+                    if utinn == 1:
+                        bubbi = int(input("Brjóta eða byggja vegg (1-2): "))
+                        fj = int(input("Hversu margir eru veggirnir? "))
+                        if bubbi == 1:
+
+                        elif bubbi == 2:
+
+                        else:
+                            print("Rangur innsláttur")
+                        teljari = 0
+                        jolasvein = []
+                        for x in range(fj):
+                            teljari += 1
+                            print("--- Veggur", teljari, "---")
+                            sterd = int(input("Sláðu inn stærð veggs í fermetrum "))
+                            jolasvein.append(sterd)
+                        print(jolasvein)
+                    elif utinn == 2:
+                        print("Staðsetning:"
+                              "\n1. Eldhúsið"
+                              "\n2. Baðherberginu")
+                        berg = int(input(">>"))
+                        if berg == 1:
+                            kost = "kost"
+                        elif berg == 2:
+                            kost = "kostinn"
+                        else:
+                            print("Rangur innsláttur")
+                    elif utinn == 3:
+                        jolakotturinn = int(input("Stærð gólfs í fermetrum: "))
+
+                    else:
+                        print("Rangur innsláttur")
         else:
             raise ValueError("Rangt gagnatak")
 
@@ -129,6 +211,7 @@ while asd:
         print(x, "\n")
         pass
         asd = True
+
 
 
 
